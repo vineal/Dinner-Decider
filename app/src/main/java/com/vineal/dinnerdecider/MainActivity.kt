cca42898924a76c3777b95be9767af01c6d91d30
Vineal Viji Varghese <vinealviji@gmail.com>
2020-12-12 12:18:46 +0530

    Completed the app. Selects food randomly. Can add list of options to choose from.

--- app/src/main/java/com/vineal/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/vineal/dinnerdecider/MainActivity.kt
@@ -3,14 +3,24 @@ package com.vineal.dinnerdecider
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    val foodList = arrayListOf("Chinese","Ham Burger","Pizza", "Mc Donalds", "Pasta")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        selectedFoodTxt.text = "Burger King"
-        addFoodBtn.setOnClickListener(){
-            selectedFoodTxt.text = addFoodTextField.text
+        selectedFoodTxt.text = getString(R.string.initial_food)
+        decideBtn.setOnClickListener(){
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
         }
+        addFoodBtn.setOnClickListener(){
+            val newFood = addFoodTextField.text.toString()
+            foodList.add(newFood)
+            addFoodTextField.text.clear()
+        }
+
     }
 }
